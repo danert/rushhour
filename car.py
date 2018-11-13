1d6b113ee1aa2053d10209b05e74dd4df0dffdc0
Farginda <Farginda@wcw-stud-145-109-63-245.wireless.uva.nl>
2018-11-13 14:15:18 +0100

    test

--- car.py
+++ car.py
@@ -8,9 +8,6 @@ class Car:
         # horizontale of verticale rijrichting
         self.direction = direction
 
-        # boolean die aangeeft of auto kan bewegen
-        self.canmove
-
     def move(self, distance):
 
         if self.direction == horizontal:
