61d58116a08954698f6abd9d1cb272e14014b086
Duncan <duncandag@gmail.com>
2018-11-15 12:10:15 +0100

    donderdag

--- car.py
+++ car.py
@@ -11,7 +11,28 @@ class Car:
     def set_coordinates():
 
         # number of first coordinate
-        list_postition = board.width * self.y + self.x
+        list_position = board.width * self.y + self.x
 
+        # set coordinate as taken in list
+        board.coordinates[list_position] = True
 
-        board.coordinates[list_postition] = True
+        # calculate other coordinates belonging to cars
+        if self.length == 2 and self.direction == hor:
+            list_position += 1
+            board.coordinates[list_position] = True
+
+        elif self.length == 2 and self.direction == ver:
+            list_position += board.width
+            board.coordinates[list_position] = True
+
+        elif self.length == 3 and self.direction == hor:
+            list_position += 1
+            board.coordinates[list_position] = True
+            list_position += 1
+            board.coordinates[list_position] = True
+
+        elif self.length == 3 and self.direction == ver:
+            list_position += board.width
+            board.coordinates[list_position] = True
+            list_position += board.width
+            board.coordinates[list_position] = True
